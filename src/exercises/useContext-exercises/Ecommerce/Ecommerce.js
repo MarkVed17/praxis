bb5adc420a3b8841cda742a25da2d841f60b6db0
Vedant Lahane <59828215+MarkVed17@users.noreply.github.com>
2022-07-19 23:55:10 +0530

    feat: consumed `cart-context` in `ECommerce` app

--- src/exercises/useContext-exercises/Ecommerce/Ecommerce.js
+++ src/exercises/useContext-exercises/Ecommerce/Ecommerce.js
@@ -1,12 +1,17 @@
+import { useContext } from "react";
+import { CartContext } from "./cart-context";
+
 export function ProductListing() {
   return ["1", "2", "3", "4"].map((item) => <h2 key={item}>Product {item}</h2>);
 }
 
 export function Cart() {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <>
       <h1>eCommerce</h1>
-      <h1> Items in cart </h1>
+      <h1> Items in cart ({cartItems})</h1>
     </>
   );
 }
